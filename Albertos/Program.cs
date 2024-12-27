e827b6fe6b25aca52ebd8a6e641b5ad0cf03f6f8
AlbertDailo <allianzone21@gmail.com>
2024-12-27 23:59:36 +0100

    My first Csharp

---
+++ Albertos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Albertos
+{
+    public static void Main(string[] args)
+    {
+        string name,location,education;
+        
+        Console.Write("WHAT IS YOUR NAME?: ");
+        name= Console.ReadLine();
+        Console.WriteLine("Hi my name is "+ name);
+        Console.ReadKey();
+       
+        Console.Write("WHERE ARE YOU FROM?: ");
+        location= Console.ReadLine();
+        Console.WriteLine("I am from "+location);
+        Console.ReadKey();
+        
+        Console.Write("WHERE YOU STUDY?: ");
+        education= Console.ReadLine();
+        Console.WriteLine("I STUDY FROM "+ education);
+        Console.ReadKey();
+
+    }
+}
